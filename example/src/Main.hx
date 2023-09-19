d57afd0fa7c916ae0a8c5217248bbf7a0bfb2cd7
Aymeric Bizouarn <aymeric.bizouarn@hotmail.fr>
2023-09-19 12:46:51 +0200

    Add move function

--- example/src/Main.hx
+++ example/src/Main.hx
@@ -7,67 +7,74 @@ import js.Browser;
 import react_beautiful_dnd.DragDropContext;
 import react_beautiful_dnd.Droppable;
 import react_beautiful_dnd.Draggable;
+import react_beautiful_dnd.DraggableLocation;
+import react_beautiful_dnd.DropResult;
 
-private typedef OuterProps = {
+import uuid.Uuid;
+
+private typedef Props = {
+  var itemSource: Array<String>;
+}
+private typedef State = {
+  var source: Array<String>;
 }
 
-class App extends ReactComponent {
-  
-  // Normally you would want to split things out into separate components.
-  // But in this example everything is just done in one place for simplicity
-  override function render()  {
-    var source:Array<String> = ["TEST 1", "TEST 2", "TEST 3"];
+class App extends react.ReactComponentOf<Props, State> {
 
-    var onDragEnd = function (result) {
-      trace(result);
-      // dropped outside the list
-      if (!result.destination) {
+  public function new(props: Props) {
+    super(props);
+    var source = ["TEST 1", "TEST 2", "TEST 3", "TEST 4", "TEST 5", "TEST 6"];
+    state = {
+      source: source
+    };
+  }
+
+  override function render() {
+    var onDragEnd = function (result: DropResult) {
+
+      if (result.destination == null) {
         return;
       }
 
-      trace("index", source);
-      var index = 0;
-      var item = null;
-      for (i in 0...source.length) {
-        if(source[i] == item){
-          index = i;
-          item = result.source[i];
-          break;
-        }
-      }
-      trace("index", (result.destination.index ));
-      var removedItem:Int = index; // Retire le trésor (élément 3) de l'index 2
-      source.insert(0, item);
+      var indexSource: Int = Std.int(result.source.index);
+      var indexDestination: Int = Std.int(result.destination.index);
+
+      var elem = state.source[indexSource];
+      state.source.remove(elem);
+      state.source.insert(indexDestination, elem);
+
+      setState({ source: state.source });
     };
 
-    var renderItem = function(provided, snapshot, item){
+    var renderItem = function (provided, snapshot, item) {
       return jsx(
         <div
           ref={provided.innerRef}
           {...provided.draggableProps}
           {...provided.dragHandleProps}
         >
-          <h1>{item}</h1>
+          <p>{item}</p>
         </div>
       );
     }
 
-    var renderDrag = function(item, source){
+    var renderDrag = function (item, source) {
       var index = 0;
       for (i in 0...source.length) {
-        if(source[i] == item){
+        if (source[i] == item) {
           index = i;
           break;
         }
       }
+      var id = Uuid.nanoId();
       return jsx(
-        <Draggable draggableId={item} index={index}>
+        <Draggable key={item} draggableId={item} index={index}>
           {(provided, snapshot) -> renderItem(provided, snapshot, item)}
         </Draggable>
       );
     }
 
-    var renderDrop = function(provided, snapshot, source){
+    var renderList = function (provided, snapshot, source) {
       return jsx(
         <div
           {...provided.droppableProps}
@@ -78,24 +85,24 @@ class App extends ReactComponent {
       );
     }
 
-    return 
-      jsx(
-        <div>
-          <h1>TEST</h1>
-          <DragDropContext onDragEnd={onDragEnd}>
-            <Droppable droppableId="droppable">
-              {(provided, snapshot) -> renderDrop(provided, snapshot, source)}
-            </Droppable>
-          </DragDropContext>
-        </div>
+    var renderDragDropContext = function (source) {
+      return jsx(
+        <DragDropContext onDragEnd={onDragEnd}>
+          <Droppable droppableId="droppable">
+            {(provided, snapshot) -> renderList(provided, snapshot, source)}
+          </Droppable>
+        </DragDropContext>
       );
+    }
+
+    return jsx(
+      <div>{state.source != null && (return renderDragDropContext(state.source));}</div>
+    );
   }
 }
 
-class Main
-{
-	public static function main()
-	{
-		ReactDOM.render(jsx('<App/>'), Browser.document.getElementById('root'));
-	}
+class Main {
+  public static function main() {
+    ReactDOM.render(jsx('<App/>'), Browser.document.getElementById('root'));
+  }
 }
--- src/react_beautiful_dnd/DropResult.hx
+++ src/react_beautiful_dnd/DropResult.hx
@@ -2,7 +2,7 @@ package react_beautiful_dnd;
 
 typedef DropResult = {
 	var reason : DropReason;
-	var destination : Null<DraggableLocation>;
+	var destination :Null<DraggableLocation>;
 	var combine : Null<Combine>;
 	var mode : MovementMode;
 	var draggableId : String;
