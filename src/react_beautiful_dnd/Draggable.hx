a23a0ebe74b4347f847f4b2dd80a31e4f59fdb85
Aymeric Bizouarn <aymeric.bizouarn@hotmail.fr>
2023-09-20 16:19:40 +0200

    Fix Draggable

--- src/react_beautiful_dnd/Draggable.hx
+++ src/react_beautiful_dnd/Draggable.hx
@@ -1,5 +1,17 @@
 package react_beautiful_dnd;
 
-@:jsRequire("react-beautiful-dnd", "Draggable") extern class Draggable extends react.ReactComponent {
+typedef DraggableProps = {
+	var draggableId : String;
+	var index : Float;
+	function children(provided:DraggableProvided, snapshot:DraggableStateSnapshot, rubric:DraggableRubric):react.ReactElement;
+	@:optional
+	var isDragDisabled : Bool;
+	@:optional
+	var disableInteractiveElementBlocking : Bool;
+	@:optional
+	var shouldRespectForcePress : Bool;
+};
+
+@:jsRequire("react-beautiful-dnd", "Draggable") extern class Draggable extends react.ReactComponentOfProps<DraggableProps> {
 	static var prototype : Draggable;
 }
